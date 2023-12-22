6a5cacdb160ed1fd1754f546d61affe6d6dfd9d1
Laura <137251441+taviral55@users.noreply.github.com>
2023-12-22 14:34:16 -0800

    fixed mongo password

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://taviral55:Efisinsauto#5@cluster0.yd2b7s3.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://taviral55:Efisinsauto5@cluster0.yd2b7s3.mongodb.net/bookengine?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
