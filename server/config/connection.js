22dfab9f88e1a63d248997e943325bcd35de4325
Laura <137251441+taviral55@users.noreply.github.com>
2023-12-22 13:53:04 -0800

    correct mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://shehzadamin:ccDRl6jnuMIbmeSD@cluster0.m5i1btj.mongodb.net/bookengine?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://taviral55:Efisinsauto#5@cluster0.yd2b7s3.mongodb.net/');
 
 module.exports = mongoose.connection;
